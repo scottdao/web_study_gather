8cd436af903528c14a0ef3dc190429c46a4000ee
scottliu <2997135288@qq.com>
2018-05-17 18:32:10 +0800

    code2vue

--- vue/src/components/index/shopCity.vue
+++ vue/src/components/index/shopCity.vue
@@ -1,6 +1,10 @@
 <template>
 	<div id="shopeCity">
 		商城
+		<div >
+			{{timer}}s
+		</div>
+		<button @click="timerStart">start</button>
 	</div>
 </template>
 
@@ -9,14 +13,45 @@
 		name: 'ShopeCity',
 		data(){
 			return{
-				
+				timer:12,
+				tFlag:true
 			}
 		},
-		activated(){
-			console.log('ShopeCity缓存啦')
+		
+		mounted(){
+		
 		},
-		deactivated(){
-			console.log('ShopeCity停用啦')
+		destroyed(){
+			clearInterval(this.djs)
+		},
+		methods:{
+			timerStart(){
+			let t = this.timer;
+			let $this = this;
+			if(this.tFlag == true){
+					this.djs = setInterval(function(){
+						if(t>0){
+							if(t<10){
+								$this.timer = 0+''+t
+							}else{
+								$this.timer = t
+							}
+							t--;
+						}else{
+							$this.timer = 60;
+							clearInterval(this.djs);
+							this.tFlag = true
+						}
+					},1000)
+					this.tFlag = false;
+			
+			}else{
+				clearInterval(this.djs)
+				this.tFlag = true
+				$this.timer = 60
+			}
+		
+		}
 		}
 	}
 </script>
