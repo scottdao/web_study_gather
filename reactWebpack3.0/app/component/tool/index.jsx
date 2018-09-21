649dc352eca60ec82b4f53a5f4a6e61a453c9ec6
scottliu <2997135288@qq.com>
2018-09-21 12:20:34 +0800

    arm

--- reactWebpack3.0/app/component/tool/index.jsx
+++ reactWebpack3.0/app/component/tool/index.jsx
@@ -7,7 +7,7 @@ Tool.prototype.post = function(url,data,callBack,failer){
 	console.log(typeof arguments[2]);
 	if(typeof arguments[1] ==='object')data = arguments[1];
 
-	else if(typeof arguments[1] === 'function')data = {};
+	else if(typeof arguments[1] === 'function')data = {};callBack = arguments[1];
 	http.post.call(this,url,data,callBack,failer);
 };
 
