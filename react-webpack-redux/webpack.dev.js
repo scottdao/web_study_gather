1c78804f0d900be550f04cc094d8772040447d4d
scott <13466484073@139.com>
2018-12-06 13:31:55 +0800

    add openbrowser

--- react-webpack-redux/webpack.dev.js
+++ react-webpack-redux/webpack.dev.js
@@ -1,6 +1,7 @@
 const webpack = require('webpack');
 const merge = require('webpack-merge');
 const common = require('./webpack.common.js');
+const openBrowserPlugin = require('open-browser-webpack-plugin');
 module.exports = merge(common, {
     devtool: 'inline-source-map',
     module: {
@@ -29,6 +30,7 @@ module.exports = merge(common, {
     },
     plugins: [
         new webpack.NamedModulesPlugin(),
-        new webpack.HotModuleReplacementPlugin()
+        new webpack.HotModuleReplacementPlugin(),
+	new webpack.openBrowserPlugin({url:'localhost:4000',browser:'chrome',delay:0})
     ]
 });
