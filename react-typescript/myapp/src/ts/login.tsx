f23f07b40513b2e52f5bd974327e8dadc8c419c2
scott <13466484073@139.com>
2019-02-01 15:36:50 +0800

    add .

--- react-typescript/myapp/src/ts/login.tsx
+++ react-typescript/myapp/src/ts/login.tsx
@@ -1,5 +1,7 @@
 import * as React from "react";
 import {Link } from 'react-router-dom';
+// import {Button} from 'antd-mobile';
+
 class Login extends React.Component<{},{}> {
     
   public state = {
@@ -13,7 +15,8 @@ class Login extends React.Component<{},{}> {
 		<div>
 		登录
 		<Link to='./index'>进入首页</Link>
-		
+	{//	<Button>点击进入</Button>\
+	}
 		</div>
 	 )
  }
