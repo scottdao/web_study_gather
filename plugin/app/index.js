4fc90f6fbabec8601f598e9e8878479539dab9d4
scottdao <13466484073@139.com>
2019-04-19 18:21:11 +0800

    add 添加插件代码

---
+++ plugin/app/index.js
@@ -0,0 +1,13 @@
+// import moment from './scott-plg';
+// console.log(moment)
+// (function(gobal, $){
+// 	console.log(gobal);
+// 	console.log($);
+// })(this, window)
+//console.log(this);
+//console.log(gobal);
+
+const plused =a=>a+1;
+const mult = a=>a*2;
+console.log(define('2333'));
+		
---
+++ plugin/app/scott-plg.js
@@ -0,0 +1,9 @@
+(function(gobal, fn) {
+	console.log(gobal);
+	typeof exports === 'object' && typeof module !== undefined ? module.exports = fn() : typeof define === 'function' &&
+		define.amd ? define(fn) : gobal.moment = fn();
+})(this, function() {
+// 	return {
+// 		a:'lsdkf'
+// 	}
+})
