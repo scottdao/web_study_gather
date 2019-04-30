720f202ddd990214ac2ae77afa9425e5256b4be2
scottdao <13466484073@139.com>
2019-04-30 13:29:14 +0800

    add 添加插件

--- plugin/app/scott-plg.js
+++ plugin/app/scott-plg.js
@@ -3,7 +3,55 @@
 	 * 区别运行环境，node/amd/window
 	 */
 	typeof exports === 'object' && typeof module !== undefined ? module.exports = fn() : typeof define === 'function' &&
-		define.amd ? define(fn) : gobal.moment = fn();
+		define.amd ? define(fn) : gobal.plg = fn();
 })(this, function() {
+	function isArray(array) {
+		return array instanceof Array || Object.prototype.toString.call(array) === ['object Array'];
+	}
 
+	function isObject(object) {
+		//null为空对象
+		return object !== null && Object.prototype.toString.call(value) === ['object Object'];
+	}
+
+	function isEmptyObject(emptyObject) {
+		//keys,可枚举；
+		//getOwnPropertyNames，可枚举与不可枚举；
+		if (Object.getOwnPropertyNames) {
+			return Object.getOwnPropertyNames(value).length === 0;
+		} else {
+			for (var i in emptyObject) {
+				if (emptyObject.hasOwnProperty(i)) {
+					return false;
+				}
+			}
+			return true;
+		}
+	}
+	function isEmptyArray(emptyArray){
+		
+		return value.length === 0?true:false;
+	}
+	/*
+	 *判别empty，空Array,object,number,string,undefiend
+	 */
+	function isEmpty(value) {
+		if (value == null) {
+			return true;
+		} else if (isObject) {
+			return isEmptyObject(value);
+		} else if (isArray(value)) {
+			return isEmptyArray(value)
+		} else {
+			return false;
+		}
+	}
+
+	return {
+		isEmpty,
+		isArray,
+		isObject,
+		isEmptyObject,
+		isEmptyArray
+	}
 })
