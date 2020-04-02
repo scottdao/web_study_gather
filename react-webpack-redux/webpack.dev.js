4695a8655ca50a80fcc2d62d62ed6efe1e760912
daoyunliu <daoyun.liu@unicareer.com>
2020-04-02 16:17:34 +0800

    add:修改配置及升级node-sass

--- react-webpack-redux/webpack.dev.js
+++ react-webpack-redux/webpack.dev.js
@@ -4,53 +4,52 @@ const common = require('./webpack.common.js');
 const openBrowserPlugin = require('open-browser-webpack-plugin');
 
 const apiServer = {
-    port:4000,
-    host:'localhost'
-}
+  port: 4000,
+  host: 'localhost'
+};
 module.exports = merge(common, {
-    devtool: 'inline-source-map',
-    output:{
-        publicPath:'/',
-    },
-    module: {
-        rules: [{ test: /\.(css|scss)$/, use: ['style-loader', 'css-loader', 'sass-loader'] }]
-    },
-   
-    devServer: {
-        contentBase: './dist',
-        hot: true,
-        port: apiServer.port,
-        inline: true,
-        //host: '192.168.0.108',
-        host:apiServer.host,
-        historyApiFallback: true,
-        noInfo: false,
-        proxy: { //通过代理解决本地跨域
-            '/api': {
-                target: 'http://api.wawa.kinlink.cn/',
-                changeOrigin: true,
-                pathRewrite: {
-                    '^/api': '/' //这里理解成用‘/api’代替target里面的地址，组件中我们调接口时直接用/api代替
-                        // 比如我要调用'http://0.0:300/user/add'，直接写‘/api/user/add’即可 代理后地址栏显示/
-                }
-            }
-        }
-    },
-    plugins: [
-        new webpack.NamedModulesPlugin(),
-        new webpack.HotModuleReplacementPlugin(),
-	    new openBrowserPlugin({
-            url:`http://${apiServer.host}:${apiServer.port}`,
-<<<<<<< HEAD
-           
-        })
-=======
-            browser:'Google Chrome',
-            delay:0,
-            ignoreErrors:'true'
-        })
-		
->>>>>>> acb5935779078324e8d1f4038dcf6e45b6b89b3f
-
+  devtool: 'inline-source-map',
+  output: {
+    publicPath: '/'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(css|scss)$/,
+        use: ['style-loader', 'css-loader', 'sass-loader']
+      }
     ]
+  },
+
+  devServer: {
+    contentBase: './dist',
+    hot: true,
+    port: apiServer.port,
+    inline: true,
+    //host: '192.168.0.108',
+    host: apiServer.host,
+    historyApiFallback: true,
+    noInfo: false,
+    proxy: {
+      //通过代理解决本地跨域
+      '/api': {
+        target: 'http://api.wawa.kinlink.cn/',
+        changeOrigin: true,
+        pathRewrite: {
+          '^/api': '/' //这里理解成用‘/api’代替target里面的地址，组件中我们调接口时直接用/api代替
+          // 比如我要调用'http://0.0:300/user/add'，直接写‘/api/user/add’即可 代理后地址栏显示/
+        }
+      }
+    }
+  },
+  plugins: [
+    new webpack.NamedModulesPlugin(),
+    new webpack.HotModuleReplacementPlugin(),
+    new openBrowserPlugin({
+      url: `http://${apiServer.host}:${apiServer.port}`,
+      browser: 'Google Chrome',
+      delay: 0,
+      ignoreErrors: 'true'
+    })
+  ]
 });
