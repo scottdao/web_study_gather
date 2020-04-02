adb0932fceb45fa53bb803cb1101ec43a6e730df
daoyunliu <daoyun.liu@unicareer.com>
2020-04-02 18:23:28 +0800

    配置

--- react-webpack-redux/app/index.jsx
+++ react-webpack-redux/app/index.jsx
@@ -1,11 +1,11 @@
-import React,{Component} from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
-import 'Style/reset.scss'
-import RouterIndex from 'Router/router'
-import {Provider,connect} from 'react-redux';
+import 'Style/reset.scss';
+import RouterIndex from 'Router/router';
+import { Provider, connect } from 'react-redux';
 import createHistory from 'history/createBrowserHistory';
-import store from 'Component/store'
-import {Switch,Redirect} from 'react-router'
+import store from 'Component/store';
+import { Switch, Redirect } from 'react-router';
 import 'antd/dist/antd.css';
 import {
   Router,
@@ -13,39 +13,45 @@ import {
   BrowserRouter,
   Route,
   Link
-} from 'react-router-dom'
-import http from 'Component/http'
+} from 'react-router-dom';
+import http from 'Component/http';
 class Index extends Component {
   constructor(props) {
     super(props);
-    this.state = {count: 1}
+    this.state = { count: 1 };
   }
- componentDidMount(){
-	// http.post('/V2/Share',{},(res) =>{
-  // 		console.log(res)
-  // })
+  componentDidMount() {
+    // http.post('/V2/Share',{},(res) =>{
+    // 		console.log(res)
+    // })
   }
   Name = () => {
     console.log(111);
-  }
+  };
   shouldComponentUpdate(nextProps, nextState) {
     return false;
   }
 
   render() {
-  	  const history = createHistory()
-      //console.log(process.env.NODE_ENV);
-     const ConfigRouter = () =>{
-      return  process.env.NODE_ENV==='development'? (<BrowserRouter >
-                  <RouterIndex />
-              </BrowserRouter>):(<HashRouter >
-                  <RouterIndex />
-              </HashRouter>)
-     }
-      return(<Provider store={store}>
-              <ConfigRouter />
-            </Provider>)
+    const history = createHistory();
+    console.log(React, 'react');
+    const ConfigRouter = () => {
+      return process.env.NODE_ENV === 'development' ? (
+        <BrowserRouter>
+          <RouterIndex />
+        </BrowserRouter>
+      ) : (
+        <HashRouter>
+          <RouterIndex />
+        </HashRouter>
+      );
+    };
+    return (
+      <Provider store={store}>
+        <ConfigRouter />
+      </Provider>
+    );
   }
 }
 
-ReactDOM.render(<Index />,document.getElementById('root'));
+ReactDOM.render(<Index />, document.getElementById('root'));
