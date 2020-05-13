b59afbea458a8b8cf47ce28c31b33a84a7f825aa
daoyunliu <daoyun.liu@unicareer.com>
2020-05-13 17:24:24 +0800

    react-cli

--- react-cli/bin/cli-init.js
+++ react-cli/bin/cli-init.js
@@ -4,21 +4,9 @@ const ir = require('inquirer');
 const path = require('path');
 const fs = require('fs');
 const glob = require('glob');
+const latestVersion = require('latest-version');
 const download = require('./download');
 cm.usage('<project-name>').parse(process.argv);
-// const promptFn = (callBack) => {
-//   return ir
-//     .prompt([
-//       {
-//         name: 'projectName',
-//         message: '请输入项目名称',
-//       },
-//     ])
-//     .then((answers) => {
-//       console.log(`你输入的项目名称是：${answers.projectName}`);
-//       callBack(answers);
-//     });
-// };
 
 const projectName = cm.args[0];
 // console.log(projectName);
@@ -42,16 +30,80 @@ if (list.length) {
     console.log(`项目${projectName}已经存在`);
     return;
   }
-  rootName = projectName;
+  // rootName = projectName;
+  next = Promise.resolve(projectName);
 } else if (rootName === projectName) {
-  next = rootName = '.';
+  //  next = rootName = '.';
+  next = ir
+    .prompt([
+      {
+        name: 'buildInCurrent',
+        message:
+          '当前目录为空，且目录名称和项目名称相同，是否直接在当前目录下创建新项目？',
+        type: 'confirm',
+        default: true,
+      },
+    ])
+    .then((answer) => {
+      return Promise.resolve(answer.buildInCurrent ? '.' : projectName);
+    });
 } else {
-  rootName = projectName;
+  // rootName = projectName;
+  next = Promise.resolve(projectName);
 }
-go();
+next && go();
+
 function go() {
-  //  console.log(path.resolve(process.cwd(), path.join('.', rootName)));
-  download(rootName).then((res) => {
-    console.log(res);
+  next.then((res) => {
+    if (res !== '.') {
+      fs.mkdirSync(res);
+    }
+    return download(res)
+      .then((item) => {
+        // console.log(item);
+        return {
+          root: res,
+          downloadTemp: item,
+          name: res,
+        };
+      })
+      .then((context) => {
+        return ir.prompt([
+          {
+            name: 'projectName',
+            message: '项目的名称',
+            default: context.name,
+          },
+          {
+            name: 'projectVersion',
+            message: '项目的版本号',
+            default: '1.0.0',
+          },
+          {
+            name: 'projectDescription',
+            message: '项目的简介',
+            default: `A project named ${context.name}`,
+          },
+        ]);
+      })
+      .then((items) => {
+        return latestVersion('react-cli')
+          .then((version) => {
+            console.log(items, version);
+            return {
+              ...context,
+              metadata: {
+                ...items,
+              },
+            };
+          })
+          .then((items) => {
+            console.log(items);
+          })
+          .catch((err) => {
+            console.log(err);
+          });
+      });
   });
+  //  console.log(path.resolve(process.cwd(), path.join('.', rootName)));
 }
