0c2d5f38aeb47d8fe9d73ea66e6ba4d3c680be5d
scottdao <13466484073@139.com>
2020-07-24 09:31:47 +0800

    feat(新功能): 源码分析

--- vueTwo/index.js
+++ vueTwo/index.js
@@ -1,5 +1,38 @@
-class MyRouter {
-  constructor() {
-    this.current = null;
+// class MyRouter {
+//   constructor() {
+//     this.current = null;
+//   }
+// }
+class Vue {
+  constructor(params){
+    this.el = params.el;
+    this.data = params.data;
+    this.playLayout(params)
+    this.parseAst()
+  }
+  createElement(id){}
+  getElement(id){
+    return document.querySelector(id);
+  }
+  parseAst(html){
+    let ele = this.getElement(this.el);
+    let ele_str = ele.outerHTML;
+    ele_str = ele_str.trim();
+    // while(ele_str){
+
+    // }
+  }
+  playLayout(params){
+    // const root = document.getElementById(params.el);
+    // let text = root.innerText;
+    // console.log(root.prototype)
+    // const wd = text.replace(/^({{)(.+)(}})$/g, '$2');
+    // root.innerHTML = params.data[wd];
+  }
+
+}
+Vue.prototype.$mount = function(el){
+  if(this.el === undefined){
+    this.el = el
   }
 }
