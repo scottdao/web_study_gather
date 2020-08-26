f1e775547061fe1ea06d5517e4e71c9cdef9e00a
scottdao <13466484073@139.com>
2020-08-26 14:16:38 +0800

    add 实现双向数据绑定

--- vueTwo/index.js
+++ vueTwo/index.js
@@ -7,9 +7,11 @@ class Vue {
   constructor(params){
     this.el = params.el;
     this.data = params.data;
+   
     this.playLayout(params)
     this.parseAst()
   }
+ 
   createElement(id){}
   getElement(id){
     return document.querySelector(id);
@@ -23,16 +25,31 @@ class Vue {
     // }
   }
   playLayout(params){
-    // const root = document.getElementById(params.el);
-    // let text = root.innerText;
+    const root = document.querySelector(params.el);
+   //  console.log(root)
+    let text = root.innerText;
+    let inHtml = root.innerHTML;
     // console.log(root.prototype)
-    // const wd = text.replace(/^({{)(.+)(}})$/g, '$2');
-    // root.innerHTML = params.data[wd];
-  }
+    const wd = text.replace(/^({{)(.+)(}})$/g, '$2').replace(/({{)|(}})/g, '').split(/[\n\s]/g).filter(item=>item !=='');
+    const data = params.data;
+    for (const value of wd) {
+      for(let item in data){
+        if(item === value){
+          const reg = new RegExp(`{{${item}}}`, 'g');
+          if(reg.test(inHtml)){
+            inHtml = inHtml.replace(reg, data[item]);
+          }
+        }
+      }
+    }
 
+    root.innerHTML = inHtml;
+  }
 }
+
 Vue.prototype.$mount = function(el){
   if(this.el === undefined){
     this.el = el
   }
 }
+// Object.defineProperties
