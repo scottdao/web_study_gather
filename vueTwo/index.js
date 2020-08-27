cbb36ed61593fcc7192bf645d5fa99b7b1745245
scottdao <13466484073@139.com>
2020-08-27 11:48:05 +0800

    add 添加vue源码编写

--- vueTwo/index.js
+++ vueTwo/index.js
@@ -3,34 +3,40 @@
 //     this.current = null;
 //   }
 // }
-class Vue {
+class operateDom{
+  static rootId = null;
+  static  getElement(id){
+      id = id || this.rootId;
+      return document.querySelector(id);
+  }
+  static createElement(id){}
+}
+class EventConfig{
+
+}
+class elementNodes{
   constructor(params){
+    this.elements = params.elements;
+  }
+}
+class Vue extends EventConfig{
+  constructor(params){
+    super(params)
     this.el = params.el;
     this.data = params.data;
-   
+    // Vue.data = this.data;
     this.playLayout(params)
-    this.parseAst()
+    // this.parseAst()
   }
- 
-  createElement(id){}
-  getElement(id){
-    return document.querySelector(id);
-  }
-  parseAst(html){
-    let ele = this.getElement(this.el);
-    let ele_str = ele.outerHTML;
-    ele_str = ele_str.trim();
-    // while(ele_str){
-
-    // }
-  }
+  // static data = {}
   playLayout(params){
     const root = document.querySelector(params.el);
    //  console.log(root)
     let text = root.innerText;
     let inHtml = root.innerHTML;
-    // console.log(root.prototype)
+    console.log(inHtml);
     const wd = text.replace(/^({{)(.+)(}})$/g, '$2').replace(/({{)|(}})/g, '').split(/[\n\s]/g).filter(item=>item !=='');
+   
     const data = params.data;
     for (const value of wd) {
       for(let item in data){
@@ -52,4 +58,15 @@ Vue.prototype.$mount = function(el){
     this.el = el
   }
 }
+// Object.defineProperty(Vue.data, 'x',{
+//   enumerable: true,
+//   configurable: true,
+//   get(){
+//       return this.data.x
+//   },
+//   set(v){
+//       console.log(v);
+    
+//   }
+// })
 // Object.defineProperties
